84559c68075ee68de6503fe3fa91e1df7ccb90be
Tram Nguyen <tramnguyen1601437@gmail.com>
2019-09-09 14:50:33 +0300

    make it better

--- server.js
+++ server.js
@@ -18,12 +18,23 @@ app.use(express.json({extended: false}));
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/todo', require('./routes/api/todo'));
 
-//Adds the react production build to serve react requests
-app.use(express.static(path.join(__dirname, "client/build")));
-//React root
-app.get("*", (req, res) => {
-res.sendFile(path.join(__dirname + "client/build/index.html"));
-});
+
+// //Adds the react production build to serve react requests
+// app.use(express.static(path.join(__dirname, "client/build")));
+// //React root
+// app.get("*", (req, res) => {
+// res.sendFile(path.join(__dirname + "client/build/index.html"));
+// });
+
+// Serve static asset in production
+if(process.env.NODE_ENV==='production'){
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client', 'build','index.html'));
+    })
+}
 
 //Listen to port
 app.listen(PORT, ()=> console.log(`Listening to ${PORT}...`));
